b41e1b3ba239f14278f135285e7468df9e74f0b4
seonho Kim <seonhokim@seonhoui-MacBookPro-2.local>
2024-07-29 09:26:16 +0900

    add data

--- src/pages/wms/vr/3dLoc/3dLoc.js
+++ src/pages/wms/vr/3dLoc/3dLoc.js
@@ -13,6 +13,11 @@ import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
+//Font
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
+
 
 
 // }, 1000);
@@ -21,8 +26,10 @@ export default function Loc3d(props) {
   const menuTitle = '3D 로케이션';
   const containerRef = useRef(null);
   const sceneRef = useRef(new THREE.Scene());
+  var camera = null;
   const [objects, setObjects] = useState([]);
 
+  var floor = null; //바닥
 
 
   //창고기본정보
@@ -31,9 +38,16 @@ export default function Loc3d(props) {
     "dcCd" : "1001",
     "clientCd" : "1001",
 
-    warehouse_width : 25,
-    warehouse_length : 25,
+    //창고크기
+    warehouse_width : 26,
+    warehouse_length : 26,
     warehouse_height : 5,
+
+    
+    //창고 시작원점 위치
+    warehouse_std_x : 0,
+    warehouse_std_y : 0,
+    warehouse_std_z : 0,
   }
 
   var ModelInfo = {
@@ -51,8 +65,52 @@ export default function Loc3d(props) {
     rack_size_floor : 3, //층수
   }
 
+
+  var CameraInfo = {
+    //카메라 위치
+    camera_x : 0,
+    // camera_x : -20,
+    camera_y : 20,
+    // camera_y : 0,
+    camera_z : 0,
+  }
+
   var RackGroup = new THREE.Group();
 
+  const [dcDataList, setDcDataList] = useState([]);
+  const selectDcList = () => {
+    var data = {};
+    client.post(`/wms/sd/dc/selectDcList`, data, {})
+      .then(res => {
+        var dataList = res.data;
+        setDcDataList(dataList);
+      }).catch(error => { 
+        console.log('error = '+error); 
+      })
+  }
+
+  const [areaDataList, setAreaDataList] = useState([]);
+  const selectAreaList = () => {
+    var data = {};
+    client.post(`/wms/sd/area/selectAreaList`, data, {})
+      .then(res => {
+        var dataList = res.data;
+        setAreaDataList(dataList);
+      }).catch(error => { 
+        console.log('error = '+error); 
+      })
+  }
+  const [zoneDataList, setZoneDataList] = useState([]);
+  const selectZoneList = () => {
+    var data = {};
+    client.post(`/wms/sd/zone/selectZoneList`, data, {})
+      .then(res => {
+        var dataList = res.data;
+        setZoneDataList(dataList);
+      }).catch(error => { 
+        console.log('error = '+error); 
+      })
+  }
 
   const [locDataList, setLocDataList] = useState([]);
   const selectLocList = () => {
@@ -74,6 +132,11 @@ export default function Loc3d(props) {
       return;
     }
 
+    //창고 시작 위치 설정
+    WarehouseInfo.warehouse_std_x = -1 * (WarehouseInfo.warehouse_width / 2);
+    WarehouseInfo.warehouse_std_y = -1 * (WarehouseInfo.warehouse_length / 2);
+
+
     console.log("Warehouse");
 
     var warehouse = containerRef.current;
@@ -89,7 +152,7 @@ export default function Loc3d(props) {
     const { renderer, width, height } = setupContainer(warehouse);
 
     //카메라 생성
-    const camera = setupCamera(warehouse);
+    camera = setupCamera(warehouse);
 
     //조명 생성
     setupLight(scene);
@@ -102,9 +165,14 @@ export default function Loc3d(props) {
 
     // 바닥 추가
     addFloor(warehouse, scene);
+    addFloorGrid(warehouse, scene);
 
     // 모델 로드
     loadModelContainer(warehouse, scene);
+
+    //폰트
+    addFont();
+    addLayer(scene);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
@@ -147,16 +215,30 @@ export default function Loc3d(props) {
   // 카메라 생성
   function setupCamera(container) {
     const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    camera.position.set(50, 50, 50);
+    camera.position.set(CameraInfo.camera_x, CameraInfo.camera_y, CameraInfo.camera_z);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    camera.layers.enable(0); // enabled by default
+    camera.layers.enable(1); // enabled by default
+    camera.layers.enable(2); // enabled by default
+
     return camera;
   }
   
   // 조명 생성
   function setupLight(scene) {
     const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(50, 50, 50);
+    light.position.set(CameraInfo.camera_x, CameraInfo.camera_y, CameraInfo.camera_z);
     scene.add(light);
+  }
+
+  //폰트
+  var loaded_font = null;
+  function addFont() {
+    var loader = new FontLoader();
+    loader.load( '/images/vr/font/dohyeon_regular.json', function ( font ) {
+      loaded_font = font;
+    } );
   }
   
   // 바닥 추가
@@ -164,10 +246,15 @@ export default function Loc3d(props) {
     // 바닥의 지오메트리 및 재질을 생성
     const floorGeometry = new THREE.PlaneGeometry(container.width, container.length);
     const floorMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = -Math.PI / 2;
+
+    floor.layers.set(0);  // Add the cube to layer 1
     scene.add(floor);
-  
+  }
+
+  //바닥 격자 추가
+  function addFloorGrid(container, scene) {
     // 격자 추가
     const gridHelper = new THREE.GridHelper(container.width, container.legnth, 0x000000, 0x888888);
     gridHelper.position.y = 0.01;
@@ -235,8 +322,8 @@ export default function Loc3d(props) {
     for(let i = 0; i < locDataList.length; i++){
       addShelf({
         seq : i+1,
-        rackX : -WarehouseInfo.warehouse_width/2 - 2 + Number(locDataList[i].linCd),
-        rackZ : -WarehouseInfo.warehouse_length/2 - 2 + Number(locDataList[i].rowCd),
+        rackX : WarehouseInfo.warehouse_std_x  + Number(locDataList[i].linCd),
+        rackZ : WarehouseInfo.warehouse_std_y  + Number(locDataList[i].rowCd),
         rackWidth : ModelInfo.rack_size_width,
         rackLength : ModelInfo.rack_size_length,
         rackFloor : ModelInfo.rack_size_floor,
@@ -244,7 +331,6 @@ export default function Loc3d(props) {
     }
     sceneRef.current.add(RackGroup);
   }
-  
 
   /** 선반 추가 */
   function addShelf(rack) {
@@ -274,12 +360,35 @@ export default function Loc3d(props) {
     RackGroup.add(rackMesh);
   }
 
+  function toggleLayer(layerIndex) {
+    console.log(camera.layers)
+    if (camera.layers.enabled(layerIndex)) {
+      camera.layers.disable(layerIndex);
+    } else {
+      camera.layers.enable(layerIndex);
+    }
+  }
+
+  function addLayer(scene){
+    // Create a new mesh and add it to layer 1
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.layers.set(1);  // Add the cube to layer 1
+    // scene.add(cube);
+  }
+
   return (
     <>
       <PageTitle title={menuTitle} />
       <Grid item xs={12} style={{ height: 750, width: '100%' }}>
+        <button onClick={()=>toggleLayer(1)}>Toggle Visibility of Layer 1</button>
+
         <div ref={containerRef} style={{ width: '100%', height: '100%' }}></div>
       </Grid>
     </>
   );
 }
+
+
+
